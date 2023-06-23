f2fccd2e2260b57cdd4e1ec0a1a42e17926224eb
Diya Sundhar <diyasundhar4@gmail.com>
2023-06-23 10:33:05 -0700

    Initial Commit

---
+++ Real Example 2/Real_Example_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Example_2App.swift
+//  Real Example 2
+//
+//  Created by Diya Sundhar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Real_Example_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
